dd7327eee89f73913353f1cd26749d130e4814ee
Cody Schindler <admin@codyschindler.com>
2015-11-18 21:56:38 -0800

    initial commit

---
+++ weather-js.js
@@ -0,0 +1,16 @@
+/**
+ * FILENAME:    weather-js.js
+ * DESCRIPTION: This is the javascript for codyschindler.com/fcc/weather.
+ * REQS:        jQuery
+ * VERISON:     1.0.0
+ * UPDATED:     2015-11-18
+ * WRITTEN BY:  Cody Schindler (quiksand)
+ * ORGANIZATION: n/a
+ * CHANGELOG:  
+ * 1.0.0 (2015-11-18): Creation
+**/
+
+$(document).ready(function(){
+
+
+});
